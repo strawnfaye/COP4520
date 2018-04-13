f5f66e9ccc015c35c1eca50c58b52f073ad5fb3b
Thomas Boswell <tcboswell95@hotmail.com>
2018-04-13 01:31:29 -0400

    Insert work
    Progress on implementing linear insert

--- ctries.cpp
+++ ctries.cpp
@@ -11,26 +11,24 @@ enum NodeType
 	t_SNode		//singleton node
 };
 
-INode* root;
-
-public bool insert (KeyType key, ValueType value, int hashCode)
+template <class T>
+class KeyType
 {
-	if (root == null)
+	T value;
+	int hashCode;
+
+	KeyType(T value)
 	{
-		*root = new INode(new SNode(key, value, false));
+		this->value = value;
+		this->hashCode = computeHash(value);
 	}
-	else if (root->main == null)
+
+	T computeHash(T key)
 	{
-		//if root points to null retry insert from null root
-		*root = null;
-		insert(key, value, int hashCode);
+		// TODO: compute hashcode from key
 	}
-	else
-	{
-		//root and root reference are valid, insert as normal
-		*root.insertHelp(key, value, hashCode, 0, null);
-	}
-}
+};
+
 
 class Node
 {
@@ -41,60 +39,9 @@ class Node
 		{
 			this->type = type;
 		}
-		
-		Node lookup(KeyType k, int hashCode, int level, Node m, INode parent)
-		{
-			switch(m->NodeType)
-			{
-				case t_CNode:
-				{
-					int index = (hashCode >> level) & 0x1f);
-					int bitMap = m.bitMap;
-					int flag = 1 << index;
-					if((bitMap & flag) == 0)	// Bitmap shoes no binding
-						return NULL;
-					else	// bitmap contains a value - descend
-					{
-						std::bitset<32> foo (bitMap & (flag-1));
-						int position = foo.count();
-						int subINode = m.array[position];
-						subINode.lookup(key, hashCode, level + 5, subINode.main, this);
-					}
-					break;
-				}
-				case t_SNode:
-				{
-					if(!m.tomb)	// Singleton node
-					{
-						if(m.hashCode == hashCode && m->key == key)
-							return (AnyRef) sn.v;
-						else
-							return NULL;
-					}
-					else	// Non-live node
-					{
-						clean(parent);
-						// throw restartexception
-					}
-					break;
-				}
-				/*
-				case NULL:
-				{
-					if(parent != NULL)
-					{
-						clean(parent);
-						// throw restartexception
-					}
-					else
-						return NULL;
-				}
-				*/
-				//case null unreachable: calling method on null object results in runtime exception
-			}
-		}
-}
+};
 
+template <class T>
 class INode: public Node
 {
     Node* main;
@@ -104,76 +51,168 @@ class INode: public Node
 		INode(Node main)
 		{
 			setNodeType(t_INode);
-			this.main = &main;
+			this->main = &main;
 		}
 		
-		bool insertHelp(KeyType key, ValueType value, int hashCode, int level, Node parent)
+		bool insertHelp(KeyType key, int level, Node parent)
 		{
-			Node main = this.main;
+			Node focus = *main;
 			
-			switch (main.type)
+			if (focus == null)
 			{
-				case t_CNode:
-					CNode cNext = main;
-					// Calculate position in bitmap
-					int index = (hashCode >> level) & 0x1f;
-					int bitMap = cNext.bitMap;
-					int flag = 1 << index;
-					int mask = flag - 1;
-					std::bitset<32> foo (bitMap & mask);
-					int position = foo.count();
+				clean(parent);
+				return false;
+			}
+			else if (focus.type == t_CNode)
+			{
+				CNode cNext = focus;
+				// Calculate position in bitmap
+				int index = (hashCode >> level) & 0x1f;
+				int bitMap = cNext.bmp;
+				int flag = 1 << index;
+				int mask = flag - 1;
+				std::bitset<32> foo(bitMap & mask);
+				int position = foo.count();
 
-					if((bitMap & flag) != 0)
-					{
-						// There is a binding at the positition and it's not null - descend
-						cNext.array[pos].insert(key, value, hashCode, level + 5, this);
-					}
+				if ((bitMap & flag) != 0)
+				{
+					// There is a binding at the positition and it's not null - descend
+					CNode *cPoint = main;
+					return cPoint.array[position].insert(key, level + 5, this);
+				}
+				else
+				{
+					// No binding at position - create a new node
+					int length = arrayLength(cNext.array);
+					INode *arr = new INode[length + 1];
+					arrayCopy(cNext.array, arr);				//TODO: copy function to specifically skip [position]
+					arr[position] = new SNode();				//TODO: SNode constructor (both SNode and CNode need to redirect to INode and bind main)
+					CNode cNew = new CNode(bmp | flag, arr);
+					*main = cNew;
+					return true;
+				}
+			}
+			else if (focus.type == t_SNode)
+			{
+				SNode sNext = main;
+				if (!sNext.tomb)
+				{
+					if (sNext.key == key)
+						return true;
 					else
 					{
-						// No binding at position - create a new node
-						int length = arrayLength(cNext.array);
-						INode *arr = new INode[length+1];
-						arrayCopy(cNext.array, arr);
-						arr[length+1] = new SNode();				//TODO: SNode constructor
-						CNode cNew = new CNode(bmp | flag, arr);
+						*main = new CNode(new SNode(key), sNext);	//TODO: CNode constructor to make CNode out of two SNodes
+						return true;
 					}
-					break;
-				case t_SNode:
-					SNode sNext = main;
+				}
+				else
+				{
+					//tomb node-must be updated through parent and reattempted
+					clean(parent);
+					return false;
+				}
 			}
 		}
 };
 
+template <typename T>
 class SNode: public Node
 {
-	KeyType key;
-	ValueType value;
-    bool tomb;
+	KeyType<T> key;
+	NodeType type;
+	bool tomb;
 
-	public:
-		SNode(KeyType key, ValueType value, bool tomb)
-		{
-			setNodeType(t_SNode);
-			this->key = key;
-			this->value= value;
-			this->tomb = tomb;
-		}
+	SNode(T val, NodeType type, bool tomb)
+	{
+		this->type = type;
+		this->key = new KeyType(val);
+		this->tomb = tomb;
+	}
 };
+
 
 class CNode: public Node
 {
-    int bitMap;
-    INode* array;  // branch factor = 2^W
+	int bmp;
+	T array[];
 
-	public:
-		CNode(SNode n)
-		{
-			setNodeType(t_CNode);
-		}
+	CNode(int bmp, T *array)
+	{
+		this->bmp = bmp;
+		this->array = array;
+	}
 };
 
+Node lookup(KeyType k, int hashCode, int level, Node m, INode parent)
+{
+	switch (m->NodeType)
+	{
+	case t_CNode:
+	{
+		int index = (hashCode >> level) & 0x1f);
+		int bitMap = m.bitMap;
+		int flag = 1 << index;
+		if ((bitMap & flag) == 0)	// Bitmap shoes no binding
+			return NULL;
+		else	// bitmap contains a value - descend
+		{
+			std::bitset<32> foo(bitMap & (flag - 1));
+			int position = foo.count();
+			int subINode = m.array[position];
+			subINode.lookup(key, hashCode, level + 5, subINode.main, this);
+		}
+		break;
+	}
+	case t_SNode:
+	{
+		if (!m.tomb)	// Singleton node
+		{
+			if (m.hashCode == hashCode && m->key == key)
+				return (AnyRef)sn.v;
+			else
+				return NULL;
+		}
+		else	// Non-live node
+		{
+			clean(parent);
+			// throw restartexception
+		}
+		break;
+	}
+	/*
+	case NULL:
+	{
+	if(parent != NULL)
+	{
+	clean(parent);
+	// throw restartexception
+	}
+	else
+	return NULL;
+	}
+	*/
+	//case null unreachable: calling method on null object results in runtime exception
+	}
+}
 
-
+bool insert(KeyType key, ValueType value, int hashCode)
+{
+	if (root == null)
+	{
+		*root = new INode(new SNode(key, value, false));
+	}
+	else if (root->main == null)
+	{
+		//if root's INode points to null retry insert from null root
+		*root = null;
+		insert(key, value, hashCode);
+	}
+	else
+	{
+		//root and root reference are valid, insert as normal
+		*root.insertHelp(key, value, hashCode, 0, null);
+	}
+}
 
 
 
@@ -220,12 +259,15 @@ T remove(KeyType key, int hashCode, int level, INode parent)
 
 }
 
-public int arrayLength(T arr[])
+template <typename T>
+int arrayLength(T arr[])
 {
 	return (sizeof(arr))/(sizeof(arr[0]));
 }
 
-public void arrayCopy (T src[], T dest[])
+template <typename T>
+0
+void arrayCopy (T src[], T dest[])
 {
 	int srcLength = arrayLength(src);
 	for (int i=0; i<srcLength; i++)
